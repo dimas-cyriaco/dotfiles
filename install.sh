7239964de6cafab7ff9005e54a5921f7c9faec92
Dimas Cyriaco <dimascyiaco@gmail.com>
2016-06-24 23:45:52 -0300

    update install script

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-pacman -S --needed git
-pacman -S --needed zsh
+sudo pacman -S --needed git
+sudo pacman -S --needed zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-pacman -S --needed neovim
-pacman -S --needed tmux
+sudo pacman -S --needed tmux
+sudo pacman -S --needed neovim
+sudo pacman -S --needed ctags
+sudo pacman -S --needed the_silver_searcher
+sudo pacman -S --needed hub
+sudo pacman -S --needed xclip
+sudo pacman -S --needed python-pip
+sudo pip install neovim
+
+nvim +PlugInstall +qall
+nvim +UpdateRemotePlugins +qall
 
 ln -sf $(pwd)/zshrc $HOME/.zshrc
 ln -sf $(pwd)/nvim $HOME/.config/nvim
