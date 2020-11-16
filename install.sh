ace9fac6d98af7ecc77779b66164c0d4cb88914b
Dimas Cyriaco <dimascyriaco@pm.me>
2020-11-16 17:15:47 -0300

    Save

--- install.sh
+++ install.sh
@@ -2,35 +2,30 @@
 
 brew install git
 brew install neovim
-brew cask install alacritty
 brew install zplug
-# brew tap homebrew/cask-fonts
+brew cask install alacritty
 brew cask install font-sauce-code-pro-nerd-font
+brew cask install github
 
+echo "stow zsh"
 stow zsh
-stow neovim
+echo "stow nvim"
+stow nvim
+echo "stow alacritty"
 stow alacritty
-
-if [ -z "$USER_EMAIL" ]; then
-  fail "USER_EMAIL is not set"
-fi
-
-if [ -z "$USER_FULL_NAME" ]; then
-  fail "USER_FULL_NAME is not set"
-fi
-
-ssh_key_file="$HOME/.ssh/id_rsa.pub"
-if [ ! -f "$ssh_key_file" ]; then
-  ssh-keygen -t rsa
-fi
-
-cat "$ssh_key_file"
 
 sudo pip3 install pynvim
 
 nvim +call dein#install()
 nvim +UpdateRemotePlugins +qall
 
-git config --global core.editor nvim
-git config --global user.email $USER_EMAIL
-git config --global user.name $USER_FULL_NAME
+if [ -z "$NEXUS_EMAIL" ]; then
+  : ${GITHUB_USERNAME?"Need to set GITHUB_USERNAME"}
+  : ${GITHUB_ACCESS_TOKEN?"Need to set GITHUB_ACCESS_TOKEN"}
+
+  npm set registry https://nexus.apps.jusbr.com/repository/npm-all/
+  npm set email $NEXUS_EMAIL
+  npm set _auth $(echo -n "$GITHUB_USERNAME:$GITHUB_ACCESS_TOKEN" | base64)
+  npm set always-auth true
+fi
+
