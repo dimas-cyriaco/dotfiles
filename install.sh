5a408023772a36a80dcd6d64bd6643c6ccf36c46
Dimas Cyriaco <dimascyriaco@pm.me>
2020-12-24 10:01:32 -0300

    SAVEPOINT

--- install.sh
+++ install.sh
@@ -9,14 +9,18 @@ brew install ripgrep
 brew install fzf
 brew install dmux
 brew install bat
+brew install fish
+brew install git-delta
+brew install gotop
+brew install flutter
+brew install yarn
 
 brew cask install alacritty
 brew cask install font-sauce-code-pro-nerd-font
 brew cask install github
 brew cask install slack
 brew cask install notion
-brew cask install steam
-brew cask install adobe-creative-cloud
+brew cask install insomnia
 
 stow zsh
 stow nvim
