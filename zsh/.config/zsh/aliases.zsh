f75255ece3294ee4bb8aed0ff69acdf047503e08
Dimas Cyriaco <dimascyriaco@pm.me>
2021-07-25 19:56:40 -0300

    Update zsh aliases.

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,9 +1,8 @@
 alias la="lsd -Al --group-dirs first"
 alias ll="lsd -A1 --group-dirs first"
-alias eiv='nvim $HOME/dotfiles/nvim/.config/nvim/init.vim'
+alias elv='nvim $HOME/dotfiles/lvim/.config/lvim/lv-config.lua'
 alias dot='source $HOME/.zshrc'
 alias hg="history 1 | grep "
-alias otz='dmux -P flutter -s task_zen $PROJECTS_PATH/task_zen'
-alias orc='dmux -P react -s jusbrasil-rc $PROJECTS_PATH/react-components'
-alias odot='dmux -P dev -s dotfiles $HOME/dotfiles'
 alias nr='npf run'
+alias lg='lazygit'
+alias nvim='lvim'
