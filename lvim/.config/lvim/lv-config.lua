c2d6915a749021c1cea51e4735a4360ba7b43604
Dimas Cyriaco <dimascyriaco@pm.me>
2021-07-27 11:15:40 -0300

    Cleanup lv-config.lua

--- lvim/.config/lvim/lv-config.lua
+++ lvim/.config/lvim/lv-config.lua
@@ -15,14 +15,11 @@ an executable
 lvim.leader = ";"
 lvim.auto_close_tree = 0
 lvim.colorscheme = "nord"
--- lvim.completion.autocomplete = true
--- lvim.default_options.scrolloff = 999
--- lvim.default_options.timeoutlen = 100
--- lvim.default_options.wrap = true
 lvim.format_on_save = true
 
 vim.cmd("set timeoutlen=150")
 vim.cmd("set relativenumber")
+
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
@@ -30,27 +27,11 @@ lvim.builtin.nvimtree.hide_dotfiles = 0
 lvim.builtin.nvimtree.show_icons.git = 1
 -- lvim.builtin.nvimtree.side = "left"
 lvim.builtin.terminal.active = true
--- lvim.builtin.zen.active = false
--- lvim.builtin.zen.window.height = 0.90
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = "maintained"
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
-
--- python
--- lvim.lang.python.diagnostics.virtual_text = true
--- lvim.lang.python.analysis.use_library_code_types = true
--- to change default formatter from yapf to black
--- lvim.lang.python.formatter.exe = "black"
--- lvim.lang.python.formatter.args = {"-"}
-
--- go
--- to change default formatter from gofmt to goimports
--- lvim.lang.formatter.go.exe = "goimports"
-
--- javascript
--- lvim.lang.tsserver.linter = nil
 
 -- Additional Plugins
 local hop_config = {
@@ -111,14 +92,15 @@ local orgmode_config = {
 			},
 		})
 
-		require("compe").setup({
-			source = {
-				orgmode = true,
-			},
-		})
+		-- require("compe").setup({
+		-- 	source = {
+		-- 		orgmode = true,
+		-- 	},
+		-- })
 	end,
 }
 
+-- { "vhyrro/neorg" },
 lvim.plugins = {
 	{ "nvim-telescope/telescope-project.nvim" },
 	{ "karb94/neoscroll.nvim" },
@@ -135,29 +117,29 @@ lvim.plugins = {
 -- lvim.user_autocommands = {{ "BufWinEnter", "*", "echo \"hi again\""}}
 
 -- Additional Leader bindings for WhichKey
--- local mappings = {
--- 	d = { "<cmd>b#<cr>", "Switch Buffers" }
-	-- D = {
-		-- name = "Diagnostics",
-		-- d = { "<cmd>TroubleToggle lsp_document_diagnostics<cr>", "document" },
-		-- l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
-		-- q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
-		-- r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
-		-- t = { "<cmd>TroubleToggle<cr>", "trouble" },
-		-- w = { "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>", "workspace" },
-	-- },
-	-- f = { "<cmd>Telescope find_files find_command=fd,--hidden,--exclude,.git,--type,f<CR>", "Find File" },
-	-- l = {
-		-- h = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
-	-- },
-	-- t = { "<cmd>lua require'telescope'.extensions.project.project{}<cr>", "Projects" },
-	-- T = {
-		-- name = "Tabs",
-		-- c = { "<cmd>BufferCloseAllButCurrent<cr>", "Close others" },
-		-- n = { "<cmd>BufferNext<cr>", "Go to next Tab" },
-		-- p = { "<cmd>BufferPrevious<cr>", "Go to previous Tab" },
-		-- t = { "<cmd>BufferPick<cr>", "Select Tab" },
-	-- },
+-- lvim.builtin.which_key = {
+-- d = { "<cmd>b#<cr>", "Switch Buffers" },
+-- D = {
+-- name = "Diagnostics",
+-- d = { "<cmd>TroubleToggle lsp_document_diagnostics<cr>", "document" },
+-- l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+-- q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+-- r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+-- t = { "<cmd>TroubleToggle<cr>", "trouble" },
+-- w = { "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>", "workspace" },
+-- },
+-- f = { "<cmd>Telescope find_files find_command=fd,--hidden,--exclude,.git,--type,f<CR>", "Find File" },
+-- l = {
+-- h = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
+-- },
+-- t = { "<cmd>lua require'telescope'.extensions.project.project{}<cr>", "Projects" },
+-- T = {
+-- name = "Tabs",
+-- c = { "<cmd>BufferCloseAllButCurrent<cr>", "Close others" },
+-- n = { "<cmd>BufferNext<cr>", "Go to next Tab" },
+-- p = { "<cmd>BufferPrevious<cr>", "Go to previous Tab" },
+-- t = { "<cmd>BufferPick<cr>", "Select Tab" },
+-- },
 -- }
 
 require("telescope").load_extension("project")
