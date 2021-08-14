58fe1e8628311a0a1df90863c8ac1f769ef2a2ef
Dimas Cyriaco <dimascyriaco@pm.me>
2021-08-14 13:27:17 -0300

    Polybar config.

---
+++ i3/.config/i3/scripts/polybar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+# polybar default &
+MONITOR="HDMI-0" polybar --reload default &
+MONITOR="HDMI-1" polybar --reload default &
+
