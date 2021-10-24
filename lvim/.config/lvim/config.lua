1e3a148544907d2354e0e660ef4a2901b6b25f2a
Dimas Cyriaco <dimascyriaco@pm.me>
2021-10-24 15:51:08 -0300

    Lvim config.

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,109 +1,143 @@
--- general
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 lvim.leader = ";"
-lvim.auto_close_tree = 0
-lvim.colorscheme = "nord"
+-- general
 lvim.format_on_save = true
+lvim.lint_on_save = true
+lvim.colorscheme = "nord"
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+-- unmap a default keymapping
+-- lvim.keys.normal_mode["<C-Up>"] = ""
+-- edit a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- lvim.builtin.telescope.on_config_done = function()
+--   local actions = require "telescope.actions"
+--   -- for input mode
+--   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
+--   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+--   -- for normal mode
+--   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
+-- end
+
+-- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+-- }
 
 vim.cmd("set timeoutlen=150")
 vim.cmd("set relativenumber")
 vim.cmd("set spelllang=pt_br,en")
 
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.nvimtree.hide_dotfiles = 0
 lvim.builtin.nvimtree.show_icons.git = 1
 lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
 
-lvim.builtin.treesitter.ensure_installed = "maintained"
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {}
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
-local hop_config = {
-	"phaazon/hop.nvim",
-	event = "BufRead",
-	config = function()
-		require("hop").setup()
-		vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-		vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
-	end,
-}
-
-local trouble_config = {
-	"folke/trouble.nvim",
-	cmd = "TroubleToggle",
-	config = function()
-		-- local actions = require("telescope.actions")
-		local trouble = require("trouble.providers.telescope")
-
-		local telescope = require("telescope")
-
-		telescope.setup({
-			defaults = {
-				mappings = {
-					i = { ["<c-t>"] = trouble.open_with_trouble },
-					n = { ["<c-t>"] = trouble.open_with_trouble },
-				},
-			},
-		})
-	end,
-}
-
-local lsp_signature_config = {
-	"ray-x/lsp_signature.nvim",
-	config = function()
-		require("lsp_signature").on_attach()
-	end,
-	event = "InsertEnter",
-}
-
-local gitlinker_config = {
-	"ruifm/gitlinker.nvim",
-	requires = "nvim-lua/plenary.nvim",
-}
-
-local orgmode_config = {
-	"kristijanhusak/orgmode.nvim",
-	config = function()
-		require("orgmode").setup({
-			org_agenda_files = { "~/Documents/org/*" },
-			org_default_notes_file = "~/Documents/org/Notes.org",
-			org_agenda_templates = {
-				j = {
-					description = "Journal",
-					template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
-					target = "~/Documents/org/Journal.org",
-				},
-			},
-		})
-
-		-- require("compe").setup({
-		-- 	source = {
-		-- 		orgmode = true,
-		-- 	},
-		-- })
-	end,
-}
-
--- { "vhyrro/neorg" },
 lvim.plugins = {
-	{ "nvim-telescope/telescope-project.nvim" },
 	{ "karb94/neoscroll.nvim" },
-	{ "arcticicestudio/nord-vim" },
-	{ "christoomey/vim-tmux-navigator" },
 	{ "mateusbraga/vim-spell-pt-br" },
 	{ "scalameta/nvim-metals" },
-	{ "megalithic/zk.nvim" },
-	{ "romgrk/doom-one.vim" },
-  { "npxbr/gruvbox.nvim", requires = { "rktjmp/lush.nvim" } },
-  { "mboughaba/i3config.vim" },
-	lsp_signature_config,
-	hop_config,
-	trouble_config,
-	gitlinker_config,
-	orgmode_config,
+	{ "arcticicestudio/nord-vim" },
 }
+-- generic LSP settings
+-- you can set a custom on_attach function that will be used for all the language servers
+-- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+-- you can overwrite the null_ls setup table (useful for setting the root_dir function)
+-- lvim.lsp.null_ls.setup = {
+--   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
+-- }
+-- or if you need something more advanced
+-- lvim.lsp.null_ls.setup.root_dir = function(fname)
+--   if vim.bo.filetype == "javascript" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
+--       or require("lspconfig/util").path.dirname(fname)
+--   elseif vim.bo.filetype == "php" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
+--   else
+--     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
+--   end
+-- end
 
-require("zk_config")
+-- set a formatter if you want to override the default lsp one (if it exists)
+lvim.lang.python.formatters = {
+	{
+		exe = "black",
+		args = {},
+	},
+}
+-- set an additional linter
+-- lvim.lang.python.linters = {
+--   {
+--     exe = "flake8",
+--     args = {}
+--   }
+-- }
+
+-- lvim.lang.typescriptreact.formatters = { { exe = 'eslint' } }
+-- lvim.lang.typescriptreact.linters = { { exe = 'eslint' } }
+-- lvim.lang.typescript.formatters = { { exe = 'eslint' } }
+-- lvim.lang.typescript.linters = { { exe = 'eslint' } }
+
+-- Prettier configuration
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{
+		exe = "prettier",
+		filetypes = {
+			"javascriptreact",
+			"javascript",
+			"typescriptreact",
+			"typescript",
+			"json",
+			"markdown",
+		},
+	},
+})
+
+-- ESLint
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{
+		exe = "eslint",
+		filetypes = {
+			"javascriptreact",
+			"javascript",
+			"typescriptreact",
+			"typescript",
+			"vue",
+		},
+	},
+})
 
 vim.cmd([[augroup scala]])
 vim.cmd([[autocmd!]])
@@ -112,40 +146,18 @@ vim.cmd([[autocmd FileType scala,sbt lua require("metals").initialize_or_attach(
 vim.cmd([[augroup end]])
 --
 vim.g["metals_server_version"] = "0.10.2+46-e7ab8592-SNAPSHOT"
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.user_autocommands = {{ "BufWinEnter", "*", "echo \"hi again\""}}
-
--- Additional Leader bindings for WhichKey
--- lvim.builtin.which_key = {
--- d = { "<cmd>b#<cr>", "Switch Buffers" },
--- D = {
--- name = "Diagnostics",
--- d = { "<cmd>TroubleToggle lsp_document_diagnostics<cr>", "document" },
--- l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
--- q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
--- r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
--- t = { "<cmd>TroubleToggle<cr>", "trouble" },
--- w = { "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>", "workspace" },
--- },
--- f = { "<cmd>Telescope find_files find_command=fd,--hidden,--exclude,.git,--type,f<CR>", "Find File" },
--- l = {
--- h = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
--- },
--- t = { "<cmd>lua require'telescope'.extensions.project.project{}<cr>", "Projects" },
--- T = {
--- name = "Tabs",
--- c = { "<cmd>BufferCloseAllButCurrent<cr>", "Close others" },
--- n = { "<cmd>BufferNext<cr>", "Go to next Tab" },
--- p = { "<cmd>BufferPrevious<cr>", "Go to previous Tab" },
--- t = { "<cmd>BufferPick<cr>", "Select Tab" },
--- },
+-- Additional Plugins
+-- lvim.plugins = {
+--     {"folke/tokyonight.nvim"}, {
+--         "ray-x/lsp_signature.nvim",
+--         config = function() require"lsp_signature".on_attach() end,
+--         event = "InsertEnter"
+--     }
 -- }
 
-require("telescope").load_extension("project")
-require("gitlinker").setup()
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- lvim.autocommands.custom_groups = {
+--   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+-- }
 require("neoscroll").setup()
-
 vim.cmd("let g:nvim_tree_group_empty = 1")
-
-vim.cmd("nnoremap <silent> <leader>lh :lua vim.lsp.buf.signature_help()<CR>")
