efd4cfdccc88f3ef6af7667106eff7b2c21a291b
Dimas Cyriaco <dimascyriaco@pm.me>
2021-11-02 13:14:41 -0300

    Add ray-x/lsp_signature.nvim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -126,6 +126,13 @@ lvim.plugins = {
 		end,
 	},
 	{
+		"ray-x/lsp_signature.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp_signature").setup()
+		end,
+	},
+	{
 		"karb94/neoscroll.nvim",
 		config = function()
 			require("neoscroll").setup()
