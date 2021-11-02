b2dfa0e29ed5fd8651537262253e04a1a8de0378
Dimas Cyriaco <dimascyriaco@pm.me>
2021-11-02 13:13:57 -0300

    Add folke/persistence.nvim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -109,6 +109,17 @@ lvim.builtin.treesitter.highlight.enabled = true
 lvim.plugins = {
 	{ "christoomey/vim-tmux-navigator" },
 	{
+		"folke/persistence.nvim",
+		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+		module = "persistence",
+		config = function()
+			require("persistence").setup({
+				dir = vim.fn.expand(vim.fn.stdpath("config") .. "/session/"),
+				options = { "buffers", "curdir", "tabpages", "winsize" },
+			})
+		end,
+	},
+	{
 		"karb94/neoscroll.nvim",
 		config = function()
 			require("neoscroll").setup()
