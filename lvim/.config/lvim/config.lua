74a5fb58c246d013db9b6176e541f9a9e6b016dc
Dimas Cyriaco <dimascyriaco@pm.me>
2021-11-02 13:15:01 -0300

    Add JoosepAlviste/nvim-ts-context-commentstring

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -133,6 +133,10 @@ lvim.plugins = {
 		end,
 	},
 	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		event = "BufRead",
+	},
+	{
 		"karb94/neoscroll.nvim",
 		config = function()
 			require("neoscroll").setup()
