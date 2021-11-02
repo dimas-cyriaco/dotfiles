4ff5641413e0f49dc5b67db7cc3f47ee5ef2326e
Dimas Cyriaco <dimascyriaco@pm.me>
2021-11-02 13:15:28 -0300

    Add phaazon/hop.nvim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -144,6 +144,15 @@ lvim.plugins = {
 		end,
 	},
 	{
+		"phaazon/hop.nvim",
+		event = "BufRead",
+		config = function()
+			require("hop").setup()
+			vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+			vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+		end,
+	},
+	{
 		"karb94/neoscroll.nvim",
 		config = function()
 			require("neoscroll").setup()
