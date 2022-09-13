c08296cfc343d124527493c674bd3aa68d27a345
Dimas Cyriaco <dimascyriaco@pm.me>
2022-09-13 20:58:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-brew install --head neovim
-brew install alacritty
+
+brew install kitty
+brew tap homebrew/cask-fonts
+brew install font-fira-code-nerd-font
+brew install lsd
 brew install antibody
+brew install starship
+brew install stow
+brew install --head neovim
 brew install asdf
-brew install aws-cli
+brew install awscli
 brew install bat
 brew install broot
 brew install dynobase
-brew tap homebrew/cask-fonts
-brew install font-fira-code-nerd-font
 brew install fzf
 brew install lazygit
-brew install lsd
 brew install pulumi
 brew install ripgrep
-brew install starship
-brew install stow
 brew install trash
-brew install zellij
 brew install --cask karabiner-elements
 brew install --cask amethyst
 
@@ -35,20 +35,19 @@ cd code
 git clone git@github.com:helix-editor/helix.git
 cd helix
 cargo install --path helix-term
+ln -s $PWD/runtime ~/.config/helix/runtime
 
 # Install FZF extra functionality.
 /opt/homebrew/opt/fzf/install
 
-stow alacritty
+stow kitty
 stow amethyst
 stow bat
 stow broot
 stow git
 stow helix
 stow karabiner
-stow nvim
 stow starship
-stow zellij
 stow zsh
 
 # echo "127.0.0.1 local.motrix.global" | sudo tee -a /etc/hosts
