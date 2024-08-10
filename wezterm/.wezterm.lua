477f96e576316500c9dc58e01d71dd4540019f03
Dimas Cyriaco <dimascyriaco@icloud.com>
2024-08-10 14:41:50 -0300

    save

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 20
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.color_scheme = "Catppuccin Mocha"
+
+return config
